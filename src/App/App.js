900f5bc4d5eac7b30a80dd813f1c4c97e9fe67a7
Elena <e.mandrovska@gmail.com>
2021-07-20 14:55:12 +0300

    done

--- src/App/App.js
+++ src/App/App.js
@@ -44,6 +44,19 @@ export default class App extends Component {
     }))
   };
 
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts })
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  };
+
   render() {
     return (
       <Main>
